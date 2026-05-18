48e3a518b247ce6bcdc7915e164ff09a38aeeecb
Noa Cuccodoro <noa.cuccodoro@epfl.ch>
2026-05-18 13:25:24 +0200

    more conservative limits for tolerated angles

--- BBM8_body/src/RollController.cpp
+++ BBM8_body/src/RollController.cpp
@@ -1,7 +1,7 @@
 #include "RollController.hpp"
 
 RollController::RollController(float kp, float ki, float kd, int servoPin)
-    : _pid(kp, ki, kd, -45.0f, 45.0f),
+    : _pid(kp, ki, kd, -20.0f, 20.0f),
       _servoPin(servoPin),
       _targetDeg(0.0f)
 {}
