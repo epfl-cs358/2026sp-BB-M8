b92fdd23da36abb2859b5e884c2f8b6335f15703
Noa Cuccodoro <noa.cuccodoro@epfl.ch>
2026-05-21 15:48:11 +0200

    update to match the setup

--- BBM8_body/src/StateEstimator.cpp
+++ BBM8_body/src/StateEstimator.cpp
@@ -45,7 +45,7 @@ void StateEstimator::update(float dt) {
 
     // Complementary filter:
     // trust gyro short-term, correct long-term drift with accel
-    _roll  = _alpha * (_roll  + _gx_ds * dt) - (1.0f - _alpha) * accelRoll;
+    _roll  = _alpha * (_roll  - _gx_ds * dt) + (1.0f - _alpha) * accelRoll;
     _pitch = _alpha * (_pitch - _gy_ds * dt) - (1.0f - _alpha) * accelPitch;
 }
 
